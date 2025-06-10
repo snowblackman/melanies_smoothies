06446de096e9729e03a0a88921022688fb3e488e
snowblackman <146904367+snowblackman@users.noreply.github.com>
2025-06-10 20:09:46 +0900

    Update streamlit_app.py
    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,7 +12,6 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
-#session = get_active_session()
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
@@ -46,6 +44,7 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order,icon="✅")
     
+    # NEW section to display smoothiefroot nutrition information
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response.json())
